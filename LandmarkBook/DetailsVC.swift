e616469a2b922aefd42e9c9fe152cc1f9982c946
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-06 21:50:57 +0300

    veri aktarımı

---
+++ LandmarkBook/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  LandmarkBook
+//
+//  Created by Mehmet Güven on 6.07.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var landmarkLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+    }
+
+}
